5f9a41b6b39d3a64d98b723ff44af8401f10ba80
krymeer <krymeer@gmail.com>
2018-04-04 06:13:39 +0200

    Playing with CodeIgniter

---
+++ libsys/application/views/templates/header.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <title>CodeIgniter Tutorial</title>
+        <?php echo link_tag( 'css/mystyles.css' ); ?>
+    </head>
+    <body>
+        <h1><?php echo $title; ?></h1>
